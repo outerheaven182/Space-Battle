f7118ed80cc10809572c7063396dbf84c56c7394
outerheaven182 <mark.corrigan@yahoo.co.uk>
2014-03-30 03:41:03 +0100

    Work ya Bastard!
    Space Battle Setup Almost Complete!

---
+++ Spacebattle_Serenity/Firefly/Assets/Scripts/Fly1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fly1: MonoBehaviour
+{
+	//The purpose of this script is to simulate Newtonian phy
+	public float maxThrust = 10; //The maximum Thrust provided by the thruster(s) at full throttle
+	public float maxThrustforward = 10;
+	public float rollWeight = 1; //This float and the next two only serve to adjust sensitivity
+	public float pitchWeight = 1;//of the controls, and to allow calibration for more massive ships.
+	public float yawWeight = 1;//Set these 3 floats to the mass of the rigidbody for sensitive control
+	// Update is called once per frame
+	
+	void FixedUpdate ()
+	{
+		float yaw = yawWeight*Input.GetAxis("Yaw");
+		float roll = rollWeight*Input.GetAxis("Roll");
+		float pitch = pitchWeight*Input.GetAxis("Pitch");
+		Vector3 Rotation = new Vector3(pitch, roll, yaw);
+		rigidbody.AddRelativeTorque(Rotation);
+		float throttle = maxThrust *Input.GetAxis("Thrust");
+		rigidbody.AddRelativeForce(Vector3.down*throttle);
+		float throttleforward = maxThrustforward *Input.GetAxis("Thrustforward");
+		rigidbody.AddRelativeForce(Vector3.forward*throttleforward);
+		System.Console.WriteLine("input is "+ yaw.ToString()+", "+pitch.ToString()+", "+roll.ToString());
+	}
+}
---
+++ Spacebattle_Serenity/Firefly/Assets/Scripts/fly.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fly : MonoBehaviour {
+
+		public int flyingSpeed = 100;
+		public int speedChange = 20;
+		
+		public int rotateInt = 2;
+		
+		void Update()
+		{
+			
+			if (Input.GetKeyDown (KeyCode.W))
+			{
+				flyingSpeed += speedChange;
+			}
+			
+			if (Input.GetKeyDown (KeyCode.S))
+			{
+				flyingSpeed += -speedChange;
+			}
+			
+			if (Input.GetKey (KeyCode.A))
+			{
+				transform.RotateAround (transform.position, transform.up, -rotateInt);
+			}
+			
+			if (Input.GetKey (KeyCode.D))
+			{
+				transform.RotateAround (transform.position, transform.up, rotateInt);
+			}
+		}
+
+		void FixedUpdate() {
+			
+			transform.position += transform.forward * flyingSpeed * Time.deltaTime;
+			
+		}
+	
+	}
